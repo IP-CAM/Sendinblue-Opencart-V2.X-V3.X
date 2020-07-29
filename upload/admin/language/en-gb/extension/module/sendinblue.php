5191c28d43d19de6cdd376757a88542278ae4376
David <davidgalet@gmail.com>
2020-07-29 11:10:37 +0200

    Aggiunta dizzionario e twig file parziale

--- upload/admin/language/en-gb/extension/module/sendinblue.php
+++ upload/admin/language/en-gb/extension/module/sendinblue.php
@@ -3,6 +3,40 @@
 $_['heading_title']       = '<img src="https://it.sendinblue.com/wp-content/themes/sendinblue2019/assets/favicon/apple-touch-icon.png" style="width: 20px;vertical-align: top;" /> SendinBlue';
 $_['heading_title_text']  = 'SendinBlue';
 
+$_['tabs_about'] = 'About SendinBlue';
+$_['tabs_contacts_manager'] = 'Contacts Manager';
+$_['tabs_automation'] = 'Automation';
+$_['tabs_transactional_emails'] = 'Transactional Emails';
+$_['tabs_support'] = 'Support';
+$_['active_plugin'] = 'Activate your plugin';
+$_['enter_your_api_key'] = 'Enter your API v3 Key:';
+$_['title_get_api_key'] = 'Please enter your API key from your SendinBlue account and if you don\'t have it yet, please go to www.sendinblue.com and subscribe. You can then get the API key from https://my.sendinblue.com/integration?';
+$_['title_start'] = 'Getting Started';
+$_['first_step'] = '1. Create your free Sendinblue account: ';
+$_['sendinblue_url'] = 'SendinBlue.com';
+$_['create_account'] = 'Create an Account';
+$_['second_step'] = '2. Activate the SendinBlue plugin here above.';
+$_['relationships'] = 'With SendinBlue, you can build and grow relationships with your contacts and customers.';
+$_['relationships_1'] = 'Automatically sync your OpenCart opt-in contacts with your SendinBlue Account';
+$_['relationships_2'] = 'Easily create engaging, mobile-friendly emails';
+$_['relationships_3'] = 'Schedule email and text message campaigns';
+$_['relationships_4'] = 'Manage transactional emails with better deliverability, custom templates, and real-time analytics';
+$_['subtitle'] = 'Why use SendinBlue?';
+$_['subtitle_1'] = 'Reach the inbox with optimized deliverability';
+$_['subtitle_2'] = 'Unbeatable pricing - the best value in email marketing';
+$_['subtitle_3'] = 'Friendly customer support by phone and email';
+$_['active_sendinblue'] = 'Activate SendinBlue to manage contacts';
+$_['manage_contacts'] = 'Use SendinBlue to manage your contacts?';
+$_['yes'] = 'Yes';
+$_['no'] = 'No';
+$_['info_activation'] = 'If you activate this feature, your new contacts will be automatically added to SendinBlue or unsubscribed from SendinBlue. To synchronize the other way around (SendinBlue to OpenCart), you should run the url (mentioned below) each day.';
+$_['info_activation_help'] = 'Select Yes to automatically add your new contacts to your SendinBlue account. For each contact, order data will be sychronized as transactional attributes: ORDER_ID, ORDER_DATE, ORDER_PRICE.';
+$_['your_lists'] = 'Your Lists:';
+$_['info_list'] = 'Select the contact list where you want to save the contacts of your OpenCart store. By default, we have created a list \'OpenCart\' in your SendinBlue account and selected it';
+$_['title_attribute_mapping'] = 'Enable Attribute Mapping?';
+$_['info_attribute_mapping'] = 'Map fields from opencart to the desired equivalent in your SendinBlue account.';
+$_['help_attribute_mapping'] = 'If you select "Yes", you\'ll be able to match your OpenCart attributes with your SendinBlue attributes.';
+
 // Text
 $_['text_extension']      = 'Extensions';
 $_['text_module']         = 'Modules';
